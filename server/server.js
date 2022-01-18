77114f962579e1038cbabc21e1c70af82a563fa6
Juan Lavallen <juanlavallen02@gmail.com>
2022-01-18 14:02:45 -0300

    agregando ruta de usuarios

--- server/server.js
+++ server/server.js
@@ -3,10 +3,14 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const app = express();
 
+const userRoute = require('./routes/users');
+
 dotenv.config();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/users', userRoute);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
