29de2f2349f13c27e852c8df60feb622faab2e3c
Juan Lavallen <juanlavallen02@gmail.com>
2022-01-18 13:49:48 -0300

    server express

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const app = express();
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+});
