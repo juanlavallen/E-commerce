f25dde7a45d1c9ffd6d08deafe686fd1653581e8
Juan Lavallen <juanlavallen02@gmail.com>
2022-01-23 23:25:19 -0300

    login agregado

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -20,4 +20,32 @@ router.post('/register', async (req, res) => {
     }
 });
 
+router.post('/login', async (req, res) => {
+
+    try {
+        const user = await User.findOne({ username: req.body.username });
+
+        if (!user) {
+            return res.status(401).json({
+                msg: 'Wrong credentials!'
+            });
+        }
+
+        const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+        const password = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+        if (password !== req.body.password) {
+            return res.status(401).json({
+                msg: 'Wrong credentials!'
+            });
+        }
+
+        res.status(200).json(user);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
+});
+
 module.exports = router;
