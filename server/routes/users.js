b043982bb8e1cbace844cd08d4f06e72db4e703d
Juan Lavallen <juanlavallen02@gmail.com>
2022-01-23 22:43:42 -0300

    modelo de productos

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,11 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
+    res.send('Test is successfull');
+});
 
+router.post('/register', (req, res) => {
+    
 });
 
 module.exports = router
