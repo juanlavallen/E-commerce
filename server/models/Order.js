283e31590571a0f55d7308adefa85547ee10dd84
Juan Lavallen <juanlavallen02@gmail.com>
2022-01-23 22:50:02 -0300

    modelo de Orden

--- server/models/Order.js
+++ server/models/Order.js
@@ -7,7 +7,10 @@ const CartSchema = new mongoose.Schema({
             productId: { type: String },
             quantity: { type: Number, default: 1 }
         }
-    ]
+    ],
+    amount: { type: Number, required: true },
+    address: { type: Object, required: true },
+    status: { type: String, default: 'pending' }
 
 }, {
     timestamps: true
