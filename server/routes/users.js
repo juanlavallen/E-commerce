cd52b754b44eb4aed1daa3792aaa1df4d36292d1
Juan Lavallen <juanlavallen02@gmail.com>
2022-01-25 10:47:24 -0300

    route get user

--- server/routes/users.js
+++ server/routes/users.js
@@ -32,4 +32,15 @@ router.delete('/:id', verifyTokenAndAuthorization, async (req, res) => {
     }
 });
 
+router.get('/find/:id', verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
 module.exports = router
