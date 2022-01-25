9f129d63c89f171cdf02fd49cd00e601b316b05c
Juan Lavallen <juanlavallen02@gmail.com>
2022-01-25 10:40:48 -0300

    verifyTokenAndAdmin function

--- server/helpers/verify-token.js
+++ server/helpers/verify-token.js
@@ -22,7 +22,7 @@ const verifyToken = (req, res, next) => {
 
 const verifyTokenAndAuthorization = (req, res, next) => {
     verifyToken(req, res, () => {
-        if (req.user.id === req.params.id || req.user.isAdmin) { 
+        if (req.user.id === req.params.id || req.user.isAdmin) {
             next();
         } else {
             res.status(403).json({
@@ -32,4 +32,20 @@ const verifyTokenAndAuthorization = (req, res, next) => {
     });
 }
 
-module.exports = { verifyToken, verifyTokenAndAuthorization }
+const verifyTokenAndAdmin = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user.isAdmin) {
+            next();
+        } else {
+            res.status(403).json({
+                msg: 'You are not alowed to do that!'
+            });
+        }
+    });
+}
+
+module.exports = {
+    verifyToken,
+    verifyTokenAndAuthorization,
+    verifyTokenAndAdmin
+}
