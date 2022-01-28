472a30474d43f31021ad5f19ea76ad3944e10a4b
Juan Lavallen <juanlavallen02@gmail.com>
2022-01-27 23:50:11 -0300

    create product

--- server/routes/products.js
+++ server/routes/products.js
@@ -5,9 +5,9 @@ const route = Router();
 
 route.post('/', verifyTokenAndAdmin, async(req, res) => {
     const newProduct = new Product(req.body);
-
     try {
-        
+        const savedProduct = await newProduct.save();
+        res.status(201).json(savedProduct);
     } catch (err) {
         res.status(500).json(err);
     }
