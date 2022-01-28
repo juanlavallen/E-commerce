578e53bb493a81c8aaebf0669ca5195bd1320cbf
Juan Lavallen <juanlavallen02@gmail.com>
2022-01-27 23:54:37 -0300

    update product

--- server/routes/products.js
+++ server/routes/products.js
@@ -3,7 +3,7 @@ const { verifyTokenAndAdmin } = require('../helpers/verify-token');
 const Product = require('../models/Product');
 const route = Router();
 
-route.post('/', verifyTokenAndAdmin, async(req, res) => {
+route.post('/', verifyTokenAndAdmin, async (req, res) => {
     const newProduct = new Product(req.body);
     try {
         const savedProduct = await newProduct.save();
@@ -12,5 +12,22 @@ route.post('/', verifyTokenAndAdmin, async(req, res) => {
         res.status(500).json(err);
     }
 });
+
+route.put('/:id', verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: req.body
+            },
+            {
+                new: true
+            }
+        );
+        res.status(200).json(updatedProduct);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = route;
