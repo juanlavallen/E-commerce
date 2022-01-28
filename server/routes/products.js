eec0d6d490ccef17aa06403c4a6c4e9b4387a9d6
Juan Lavallen <juanlavallen02@gmail.com>
2022-01-28 00:04:47 -0300

    get all products

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,4 +1,4 @@
-const { Router } = require('express');
+const { Router, query } = require('express');
 const { verifyTokenAndAdmin } = require('../helpers/verify-token');
 const Product = require('../models/Product');
 const route = Router();
@@ -30,7 +30,7 @@ route.put('/:id', verifyTokenAndAdmin, async (req, res) => {
     }
 });
 
-route.delete('/:id', verifyTokenAndAdmin, async(req, res) => {
+route.delete('/:id', verifyTokenAndAdmin, async (req, res) => {
     try {
         await Product.findByIdAndDelete(req.params.id);
         res.status(200).json('Product has been deleted...');
@@ -39,7 +39,7 @@ route.delete('/:id', verifyTokenAndAdmin, async(req, res) => {
     }
 });
 
-route.get('/find/:id', async(req, res) => {
+route.get('/find/:id', async (req, res) => {
     try {
         const product = await Product.findById(req.params.id);
         res.status(200).json(product);
@@ -47,5 +47,28 @@ route.get('/find/:id', async(req, res) => {
         res.status(500).json(err);
     }
 });
- 
+
+route.get('/', async (req, res) => {
+    const qNew = req.query.new;
+    const qCategory = req.query.category;
+    try {
+        let products;
+
+        if (qNew) {
+            products = await Product.find().sort({ createdAt: -1 }).limit(5)
+        } else if (qCategory) {
+            products = await Product.find({
+                categories: {
+                    $in: [qCategory],
+                }
+            })
+        } else {
+            products = await Product.find();
+        }
+        res.status(200).json(products);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = route;
