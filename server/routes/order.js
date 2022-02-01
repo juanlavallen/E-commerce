5801f711dfe120c7da17f808d43e6ba4641ccf23
Juan Lavallen <juanlavallen02@gmail.com>
2022-01-31 23:40:43 -0300

    route income order finished

--- server/routes/order.js
+++ server/routes/order.js
@@ -57,8 +57,20 @@ route.get('/income', verifyTokenAndAdmin, async (req, res) => {
     const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
     try {
         const income = await Order.aggregate([
-            { $match: { createdAt: { $gte: previousMonth } } }
+            { $match: { createdAt: { $gte: previousMonth } } },
+            {
+                $project: {
+                    month: { $month: '$createdAt' },
+                    sales: '$amount'
+                },
+                {
+                $group: {
+                    _id: '$month',
+                    total: { $sum: '$sales' }
+                }
+            }
         ]);
+        res.status(200).json(income);
     } catch (err) {
         res.status(500).json(err);
     }
