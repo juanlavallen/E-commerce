1d6e39412c26d4da3f7f197c8f15703e464365cd
Juan Lavallen <juanlavallen02@gmail.com>
2022-01-31 23:15:25 -0300

    route delete order finished

--- server/routes/order.js
+++ server/routes/order.js
@@ -24,4 +24,13 @@ route.put('/:id', verifyTokenAndAdmin, async (req, res) => {
     }
 });
 
+route.delete('/:id', verifyTokenAndAdmin, async (req, res) => {
+    try {
+        await Order.findByIdAndDelete(req.params.id)
+        res.status(200).json('Order has been deleted...');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = route;
