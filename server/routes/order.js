32a076e7026bb3c6c3632a832b81fb3aaaa4b1bf
Juan Lavallen <juanlavallen02@gmail.com>
2022-01-31 23:10:57 -0300

    route create order finished

--- server/routes/order.js
+++ server/routes/order.js
@@ -1,6 +1,17 @@
 const { Router } = require('express');
+const { verifyToken } = require('../helpers/verify-token');
+const Order = require('../models/Order');
 const route = Router();
 
+route.post('/', verifyToken, async (req, res) => {
+    const newOrder = new Order(req.body);
+    try {
+        const savedOrder = await newOrder.save();
+        res.status(201).json(savedOrder);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 module.exports = route;
