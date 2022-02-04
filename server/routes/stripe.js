35a4092b6c401541210df204ff2a231aa9781d7f
Juan Lavallen <juanlavallen02@gmail.com>
2022-02-03 22:41:42 -0300

    route stripe-peyment

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -1,5 +1,15 @@
 const { Router } = require('express');
-const stripe = require('stripe')(process.env.SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_KEY);
 const route = Router();
+
+route.post('/payment', async(req, res) => {
+    stripe.charges.create({
+        source: req.body.tokenId,
+        amount: req.body.amount,
+        currency: 'usd'
+    }, (stripeErr, stripeRes) => {
+        
+    });
+});
 
 module.exports = route;
