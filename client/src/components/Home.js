a637d6def5cdd698c4e4f406239266c23bcef9cd
Juan Lavallen <juanlavallen02@gmail.com>
2022-02-04 23:21:25 -0300

    renderizando el componente Navbar

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 const Home = () => {
     return (
         <>
-            This is Home :P
+            <Navbar />
         </>
     );
 }
